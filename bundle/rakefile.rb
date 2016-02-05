e94199f25c8efc2324c277d27cbb9f98926466a6
Michael Patrick O'Keefe <michael.okeefe@bigladdersoftware.com>
2016-02-05 08:34:24 -0700

    Add alias

--- bundle/rakefile.rb
+++ bundle/rakefile.rb
@@ -10,10 +10,19 @@ task :install do
   end
 end
 
-desc "update repos"
-task :update do
+def update
   dirs = Dir['*'].reject {|f| File.file? f}
   dirs.each do |d|
     sh "cd #{d} && git pull origin master && cd .."
   end
+end
+
+desc "update repos"
+task :update do
+  update
+end
+
+desc "same as update"
+task :upgrade do
+  update
 end
